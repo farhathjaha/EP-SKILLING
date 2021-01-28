e20108faf93528dddde0ff34d371eca6b26869bc
farhathjaha <74001703+farhathjaha@users.noreply.github.com>
2021-01-28 16:28:09 +0530

    Add files via upload

---
+++ bean/Bean.java
@@ -0,0 +1,26 @@
+package bean;
+
+public class Bean {
+	private int itemid;
+	private String itemname;
+	private int itemcost;
+	public int getItemid() {
+		return itemid;
+	}
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+	public String getItemname() {
+		return itemname;
+	}
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+	public int getItemcost() {
+		return itemcost;
+	}
+	public void setItemcost(int itemcost) {
+		this.itemcost = itemcost;
+	}
+
+}
